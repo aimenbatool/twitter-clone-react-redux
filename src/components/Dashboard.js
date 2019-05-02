9c011172fe84d262be29afd800725d9031e296e5
Aimen Batool <s.aimenbatool@gmail.com>
2019-05-03 04:02:51 +0500

    Add Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Dashboard extends Component {
+    render() {
+        return(
+            <div>
+                <h3 className='center'> Your Timeline </h3>
+                <ul className='dashboard-list'>
+                    {this.props.tweetIds.map((id) => (
+                        <li key={id}>
+                            <div> TWEET ID: {id} </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = ({ tweets }) => {
+    return {
+        tweetIds: Object.keys(tweets)
+            .sort((a,b) => tweets[b].timeStamp - tweets[a].timeStamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard);
