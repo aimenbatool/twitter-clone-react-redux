f43ab4629d614eb39419ddf32c631090befd6945
Aimen Batool <s.aimenbatool@gmail.com>
2019-05-05 04:02:43 +0500

    Add TweetPage and NewTweet feature

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,33 +1,58 @@
-import { saveLikeToggle } from '../utils/api';
+import { saveLikeToggle, saveTweet } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
-export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
-export const TOGGLE_TWEET = 'TOGGLE_Tweet';
+export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
 
-export const receiveTweets = (tweets) => {
-    return {
-        type: RECEIVE_TWEETS,
-        tweets,
-    }
+function addTweet (tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet,
+  }
 }
 
-export const toggleTweet = ({ id, authedUser, hasLiked }) => {
-    return {
-        type: TOGGLE_TWEET,
-        id,
-        authedUser,
-        hasLiked,
-    }
+export function handleAddTweet (text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    dispatch(showLoading())
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then((tweet) => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()))
+  }
 }
 
-export const handleToggleTweet = (info) => {
-    return (dispatch) => {
-        dispatch(toggleTweet(info));
+export function receiveTweets (tweets) {
+  return {
+    type: RECEIVE_TWEETS,
+    tweets,
+  }
+}
 
-        return saveLikeToggle(info)
-            .catch((e) => {
-                console.warn('Error in handleToggleTweet: ', e);
-                dispatch(toggleTweet(info));
-                alert('There was an error liking the tweet. Try again.')
-            })
-    }
+function toggleTweet ({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  }
+}
+
+export function handleToggleTweet (info) {
+  return (dispatch) => {
+    dispatch(toggleTweet(info))
+
+    return saveLikeToggle(info)
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet: ', e)
+        dispatch(toggleTweet(info))
+        alert('The was an error liking the tweet. Try again.')
+      })
+  }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
+import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 
 class App extends Component {
 
@@ -17,7 +19,7 @@ class App extends Component {
         {
           this.props.loading === true
           ? null
-          : <Dashboard />
+          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />
         }
       </div>
     )
